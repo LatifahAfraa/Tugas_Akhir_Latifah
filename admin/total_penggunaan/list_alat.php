4eb9e122b6513161bdd9b491b706f8475ce7bb4c
Latifah <afralatifah@gmail.com>
2022-06-08 23:33:40 +0700

    penggunaan

--- admin/total_penggunaan/list_alat.php
+++ admin/total_penggunaan/list_alat.php
@@ -1,7 +1,3 @@
-<?php
-$data = mysqli_query($konek, "SELECT * FROM lampu");
-$lampu = mysqli_fetch_assoc($data);
-?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -30,34 +26,23 @@ $lampu = mysqli_fetch_assoc($data);
                         <!-- /.card-header -->
                         <div class="card-body">
                             <center><label>Total Penggunaan Pada Setiap Traffic Light</label></center><br><br>
-                            <div class="form-group row">
-                                <p for="lampu1" class="col-sm-6 col-form-label">Total Penggunaan Lampu Lalu Lintas 1</p>
-                                <div class="col-sm-6">
-                                    <input type="text" name="lampu1" value="<?php echo $lampu['lampu1'] ?>x" class="form-control" id="waktu_aktif1" readonly>
+                            <?php
+                            $row = mysqli_query($konek, "SELECT * FROM lampu ORDER BY id_lampu");
+                            while ($lampu = mysqli_fetch_assoc($row)) {
+                                $penggunaan = mysqli_query($konek, "SELECT *FROM penggunaan where id_lampu ='$lampu[id_lampu]'"); //$Lampu 'id_lampu' dari tabel lampu 
+                                $total_penggunaan = mysqli_num_rows($penggunaan);
+                            ?>
+                                <div class="form-group row">
+                                    <p for="lampu<?php echo $lampu['id_lampu']?>" class="col-sm-6 col-form-label">Total Penggunaan <?php echo $lampu['nama_lampu']?></p>
+                                    <div class="col-sm-6">
+                                        <input type="text" name="lampu<?php echo $lampu['id_lampu']?>" value="<?php echo $total_penggunaan; ?>x" class="form-control" id="waktu_aktif1" readonly>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="form-group row">
-                                <p for="lampu2" class="col-sm-6 col-form-label">Total PenggunaanLampu Lalu Lintas 2</p>
-                                <div class="col-sm-6">
-                                    <input type="text" name="lampu2" value="<?php echo $lampu['lampu2'] ?>x" class="form-control" id="waktu_aktif2" readonly>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <p for="lampu3" class="col-sm-6 col-form-label">Total Penggunaan Lampu Lalu Lintas 3</p>
-                                <div class="col-sm-6">
-                                    <input type="text" name="lampu3" value="<?php echo $lampu['lampu3'] ?>x" class="form-control" id="waktu_aktif3" readonly>
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <p for="lampu4" class="col-sm-6 col-form-label">Total Penggunaan Lampu Lalu Lintas 4</p>
-                                <div class="col-sm-6">
-                                    <input type="text" name="lampu4" value="<?php echo $lampu['lampu4'] ?>x" class="form-control" id="waktu_aktif4" readonly>
-                                </div>
-                            </div>
-
+                            <?php
+                            }
+                            ?>
+                            
 
                             <a id="cetak" href="" target="_blank" class="btn btn-primary" role="button" title="Cetak Data" style="float: right; margin: 30px 30px 0 0;"><i class="fas fa-print"></i> Cetak</a>
                         </div>
