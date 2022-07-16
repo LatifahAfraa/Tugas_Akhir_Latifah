9ba30e3d4f330c3af22d0ca9be81355c42c12b0d
Rahmad Arif <rahmadarif28080@gmail.com>
2022-07-16 18:22:31 +0700

    new api

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Jakarta");
-$konek = mysqli_connect('localhost', 'root', '') or die ('connect failed!!');
+$konek = mysqli_connect('localhost', 'root', '123456') or die ('connect failed!!');
 /*echo "berhasil terkoneksi.....";
 */
-mysqli_select_db($konek, 'db_ta') or die('database not found');
+mysqli_select_db($konek, 'ta_latifah') or die('database not found');
 ?>
