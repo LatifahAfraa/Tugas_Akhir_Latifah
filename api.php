c124134969c60630ecb4ba72de1ace379ff03ac3
Rahmad Arif <rahmadarif28080@gmail.com>
2022-07-19 08:36:46 +0700

    tambah waktu lampu

--- api.php
+++ api.php
@@ -1,32 +1,66 @@
 <?php
     require "koneksi/koneksi.php";
 
-    function update_lampu($is_scan=false) {
+    function update_lampu($is_scan=false, $lampu_master) {
         global $konek;
         $waktu_sekarang = date('Y-m-d H:i:s');
-        $tambahan_waktu = 60;
-        $durasi_waktu = 30;
+
+        $next_change_at = $lampu_master['next_change_at']['value'] ?? 0;
+        $latest_change_at = $lampu_master['latest_change_at']['value'] ?? 0;
+
+        $tambahan_waktu_scan = $lampu_master['tambahan_waktu_scan']['value'];
+        $durasi_waktu = $lampu_master['durasi_lampu']['value'];
 
         if($is_scan) {
-            $table_lampu_hijau = mysqli_query($konek, "SELECT * FROM lampu WHERE status_lampu='hijau' LIMIT 1");
-            $fetch_lampu_hijau = mysqli_fetch_assoc($table_lampu_hijau);
+            // $table_lampu_hijau = mysqli_query($konek, "SELECT * FROM lampu WHERE status_lampu='hijau' LIMIT 1");
+            // $fetch_lampu_hijau = mysqli_fetch_assoc($table_lampu_hijau);
+            $next_change_at = time()+$tambahan_waktu_scan;
+            $latest_change_at = time();
+            mysqli_query($konek, "UPDATE lampu_master SET value='$next_change_at' WHERE name='next_change_at'");
+            mysqli_query($konek, "UPDATE lampu_master SET value='$latest_change_at' WHERE name='latest_change_at'");
 
-            if(date("Y-m-d H:i:s") > date("Y-m-d H:i:s", strtotime($fetch_lampu_hijau['waktu_terakhir_scan'])+$tambahan_waktu)) {
-                mysqli_query($konek, "UPDATE lampu SET status_lampu='merah', waktu_terakhir_scan='$waktu_sekarang' ' WHERE id_lampu='$fetch_lampu_hijau[id_lampu]'");
-                if($fetch_lampu_hijau['id_lampu'] == 3) {
-                    mysqli_query($konek, "UPDATE lampu SET status_lampu='hijau', waktu_terakhir_scan='$waktu_sekarang' ' WHERE id_lampu='1'");
-                } else {
-                    mysqli_query($konek, "UPDATE lampu SET status_lampu='merah', waktu_terakhir_scan='$waktu_sekarang' ' WHERE id_lampu='".($fetch_lampu_hijau['id_lampu']+1)."'");
-                }
-            }
+            return $tambahan_waktu_scan;
+
+            // mysqli_query($konek, "UPDATE lampu SET status_lampu='merah', waktu_terakhir_scan='$waktu_sekarang' WHERE id_lampu='$fetch_lampu_hijau[id_lampu]'");
+            
+            // if(time() > (strtotime($fetch_lampu_hijau['waktu_terakhir_scan'])+$tambahan_waktu_scan)) {
+            //     mysqli_query($konek, "UPDATE lampu SET status_lampu='merah', waktu_terakhir_scan='$waktu_sekarang' WHERE id_lampu='$fetch_lampu_hijau[id_lampu]'");
+            //     if($fetch_lampu_hijau['id_lampu'] == 3) {
+            //         mysqli_query($konek, "UPDATE lampu SET status_lampu='hijau', waktu_terakhir_scan='$waktu_sekarang' WHERE id_lampu='1'");
+            //     } else {
+            //         mysqli_query($konek, "UPDATE lampu SET status_lampu='merah', waktu_terakhir_scan='$waktu_sekarang' WHERE id_lampu='".($fetch_lampu_hijau['id_lampu']+1)."'");
+            //     }
+            // }
 
         } else {
+
+            if($next_change_at <= time()) {
+                $next_change_at = time()+$durasi_waktu;
+                $latest_change_at = time();
+
+                $table_lampu_hijau = mysqli_query($konek, "SELECT * FROM lampu WHERE status_lampu='hijau' LIMIT 1");
+                $fetch_lampu_hijau = mysqli_fetch_assoc($table_lampu_hijau);
+                
+                mysqli_query($konek, "UPDATE lampu SET status_lampu='merah'");
+                if($fetch_lampu_hijau['id_lampu'] == 3) {
+                    mysqli_query($konek, "UPDATE lampu SET status_lampu='hijau' WHERE id_lampu='1'");
+                } else {
+                    $lampu_next_id = $fetch_lampu_hijau['id_lampu'] + 1;
+                    mysqli_query($konek, "UPDATE lampu SET status_lampu='hijau' WHERE id_lampu='$lampu_next_id'");
+                }
+                
+                mysqli_query($konek, "UPDATE lampu_master SET value='$next_change_at' WHERE name='next_change_at'");
+                mysqli_query($konek, "UPDATE lampu_master SET value='$latest_change_at' WHERE name='latest_change_at'");
+                
+                return $durasi_waktu;
+            } 
+            /*
             $table_lampu = mysqli_query($konek, "SELECT * FROM lampu ORDER BY id_lampu ASC"); 
             // $current_lampu = 
             $i = 1;
             while($fetch_lampu = mysqli_fetch_assoc($table_lampu)) {
                 
-                if($fetch_lampu['status_lampu'] == "hijau") {
+                // if($fetch_lampu['status_lampu'] == "hijau") {
                     if(time() >  strtotime($fetch_lampu['waktu_terakhir_scan'])+$durasi_waktu) {
                         if($fetch_lampu['id_lampu'] == 3) {
                             mysqli_query($konek, "UPDATE lampu SET status_lampu='merah', waktu_terakhir_scan='$waktu_sekarang' WHERE id_lampu=3");
@@ -36,12 +70,13 @@
                             mysqli_query($konek, "UPDATE lampu SET status_lampu='hijau', waktu_terakhir_scan='$waktu_sekarang' WHERE id_lampu='".($fetch_lampu['id_lampu']+1)."'");
                         }
                     }
-                }
+                // }
                 $i++;
             }
+            */
         }
 
-        return true;
+        return 0;
     }
 
     if(isset($_GET['rfid'])) {
@@ -50,6 +85,14 @@
         $lampu_3 = $_GET['device3'] ?? 0;
 
         $rfid = $_GET['rfid'];
+        $waktu = 0;
+
+        // Get Lampu Master
+        $query_lampu_master = mysqli_query($konek, "SELECT * FROM lampu_master");
+        $lampu_master = [];
+        while ($fetch_lampu_master = mysqli_fetch_assoc($query_lampu_master)) {
+            $lampu_master[$fetch_lampu_master['name']] = $fetch_lampu_master;
+        }
 
         if($rfid != 0) {
             // SIMPAN LOG SCAN
@@ -61,19 +104,19 @@
     
             if($row_user != 0) { // JIKA USER DITEMUKAN => UBAH DATA LAMPU
                 // echo json_encode($res);
-                update_lampu(true);
+                $waktu = update_lampu(true, $lampu_master);
             } else {
-                update_lampu();
+                $waktu = update_lampu(false, $lampu_master);
             }
         } else {
-            update_lampu();
+            $waktu = update_lampu(false, $lampu_master);
         }
         
 
 
         $query_lampu = mysqli_query($konek, "SELECT * FROM lampu");
         $res = [];
-        
+        $res['waktu'] = (int) $waktu;
         while ($fetch_lampu = mysqli_fetch_assoc($query_lampu)) {
             $res["lampu_".$fetch_lampu['id_lampu']] = ($fetch_lampu['status_lampu'] == "hijau")? 1 : 3;
         }
