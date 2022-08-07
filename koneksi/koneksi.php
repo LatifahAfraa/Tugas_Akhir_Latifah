dec60554c33cb00c0a3e3405b9cbb8fc43098bcc
Rahmad Arif <rahmadarif28080@gmail.com>
2022-08-07 21:06:22 +0700

    fix connection

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set("Asia/Jakarta");
 
-// $konek = mysqli_connect('db-ta.cs50f4xozssp.us-east-1.rds.amazonaws.com', 'admin', 'admin12345') or die ('connect failed!!');
-$konek = mysqli_connect('localhost', 'root', '') or die ('connect failed!!');
+$konek = mysqli_connect('db-ta.cs50f4xozssp.us-east-1.rds.amazonaws.com', 'admin', 'admin12345') or die ('connect failed!!');
+// $konek = mysqli_connect('localhost', 'root', '') or die ('connect failed!!');
 /*echo "berhasil terkoneksi.....";
 */
-mysqli_select_db($konek, 'ta_latifah') or die('database not found');
+mysqli_select_db($konek, 'db_ta') or die('database not found');
 ?>
