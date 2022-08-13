0ae22d20d5b2ad74c2f3ff148c6faf0dbcdd59b1
Latifah <afralatifah@gmail.com>
2022-08-13 22:04:27 +0700

    userr

--- nonaktif/nonaktif.php
+++ nonaktif/nonaktif.php
@@ -9,12 +9,12 @@ $user = mysqli_fetch_assoc($data);
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Mengaktifkan Atau Menonaktifkan Kartu RFID</h1>
+                    <h1>Menonaktifkan Kartu RFID</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Mengaktifkan Atau Menonaktifkan Kartu RFID</li>
+                        <li class="breadcrumb-item active">Menonaktifkan Kartu RFID</li>
                     </ol>
                 </div>
             </div>
